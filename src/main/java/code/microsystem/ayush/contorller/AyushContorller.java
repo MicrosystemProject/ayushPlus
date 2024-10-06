a07ae1da08c7325f76533806d49411f4c3dd1508
MicrosystemProject <microsystem.project@gmail.com>
2024-10-06 17:48:34 +0530

    Intial Commit

---
+++ src/main/java/code/microsystem/ayush/contorller/AyushContorller.java
@@ -0,0 +1,16 @@
+package code.microsystem.ayush.contorller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//@RequestMapping("/api/v1/ayush")
+public class AyushContorller {
+
+    @GetMapping("/health")
+    public String getHealth(){
+    return "AyushPlus api running Healthy with version 1.0";
+}
+}
---
+++ src/test/java/code/microsystem/ayushPlus/AyushPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package code.microsystem.ayushPlus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AyushPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
